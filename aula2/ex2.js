f97c8a6b25b5c42aea5cf618e08ddb2b490a747c
AleXolv <alexolv00@gmail.com>
2021-11-09 13:21:15 -0300

    aula2

---
+++ aula2/ex2.js
@@ -0,0 +1,21 @@
+//let carro = "Fiesta";
+//let Ano = 2012;
+//et usuarioAtivo = true;
+ 
+//console.log(carro);
+//console.log(Ano);
+//console.log(usuarioAtivo);
+
+let carro = "Fiesta"
+let Ano = 2012
+let usuarioAtivo = true
+ 
+console.log("Carro: ")
+console.log(carro)
+console.log("Ano: ")
+console.log(Ano)
+console.log("Usuário está ativo no sistema?")
+console.log(usuarioAtivo)
+
+
+
