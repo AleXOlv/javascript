ffeea140853ffafbb1a14e112c52535a9ecbc968
AleXolv <alexolv00@gmail.com>
2021-11-09 23:09:54 -0300

    Entrega Checkpoint 1 Javascript

--- checkpoint1/check1.js
+++ checkpoint1/check1.js
@@ -12,3 +12,78 @@
 // - Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
 // - No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
 
+    
+    function microondas(tipo, valor) {
+        let mensagem = 'Obrigado por usar Microondas DH\n';
+    
+        switch(tipo) {
+            case 'Pipoca' :
+                mensagem += 'Você selecionou Pipoca\n';
+    
+                if (valor >= 20 && valor <=29) {
+                    mensagem += 'Comida Queimou!\n';
+                }else if (valor < 10) {
+                    mensagem += 'Tempo insuficinete!!\n';
+                }else if (valor >= 30) {
+                    mensagem += 'Kabum!\n';
+                }
+            break;
+            case 'Macarrao' :
+                mensagem += 'Você selecionou Macarrao\n';
+    
+                if (valor >= 16 && valor <=23) {
+                    mensagem += 'Comida Queimou!\n';
+                }else if (valor < 8) {
+                    mensagem += 'Tempo insuficinete!!\n';
+                }else if (valor >= 24) {
+                    mensagem += 'Kabum!\n';
+                }
+            break;
+            case 'Carne' :
+                mensagem += 'Você selecionou Carne\n';
+    
+                if (valor >= 30 && valor <=44) {
+                    mensagem += 'Comida Queimou!\n';
+                }else if (valor < 15) {
+                    mensagem += 'Tempo insuficinete!!\n';
+                }else if (valor >= 45) {
+                    mensagem += 'Kabum!\n';
+                }
+            break;
+            case 'Feijao' :
+                mensagem += 'Você selecionou Feijao\n';
+    
+                if (valor >= 24 && valor <=35) {
+                    mensagem += 'Comida Queimou!\n';
+                }else if (valor < 12) {
+                    mensagem += 'Tempo insuficinete!!\n';
+                }else if (valor >= 36) {
+                    mensagem += 'Kabum!\n';
+                }
+            break;
+            case 'Brigadeiro' :
+                mensagem += 'Você selecionou Brigadeiro\n';
+    
+                if (valor >= 16 && valor <=23) {
+                    mensagem += 'Comida Queimou!\n';
+                }else if (valor < 8) {
+                    mensagem += 'Tempo insuficinete!!\n';
+                }else if (valor >= 24) {
+                    mensagem += 'Kabum!\n';
+                }
+            break;
+            
+            
+            
+            default :
+                mensagem += 'Prato inexistente!\n';
+        }
+    
+        
+    
+        return mensagem;
+    
+    }
+console.log(microondas('Macarrao', 7));
+console.log("Prato pronto, bom apetite");
+
